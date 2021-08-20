d0ce62850baa26fb647a5f1d12d65593d7c18e94
Sam Niyazi <24701208+SamNiyazi2@users.noreply.github.com>
2021-08-20 15:39:26 -0500

    Add project files.

---
+++ YT-335-MVCDemo-v02/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace YT_335_MVCDemo_v02.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
